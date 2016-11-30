02f3d8f1f362d4f60846ca4b40bf7bb30471c827
Lamine Ndiaye <lamine.ndy@gmail.com>
2016-11-30 21:04:08 +0100

    fetch albums

--- QontoTest/Classes/Model/User.swift
+++ QontoTest/Classes/Model/User.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class User: NSObject {
+final class User: NSObject, ResponseObjectSerializable, ResponseCollectionSerializable {
   
   var userId: Int?
   var name: String?
@@ -19,4 +20,35 @@ class User: NSObject {
     self.name = name
     self.userName = userName
   }
+  
+  override init() {
+  }
+  
+  //Mark:- ResponseObjectSerializable, ResponseCollectionSerializable methods
+  required convenience init?(response: HTTPURLResponse, representation: AnyObject) {
+    self.init()
+    self.populateFromResult(representation)
+  }
+  
+  static func collection(from response: HTTPURLResponse, withRepresentation representation: Any) -> [User] {
+    var users = [User]()
+    if let jsonValues = representation as? [[String: AnyObject]] {
+      for storeRepresentation in jsonValues {
+        if let user = User(response: response, representation: storeRepresentation as AnyObject) {
+            users.append(user)
+        }
+      }
+    }
+    return users
+  }
+  
+  func populateFromResult(_ representation: AnyObject?) {
+    if let jsonRepresentation = representation {
+      let json = JSON(jsonRepresentation)
+      userId = json["id"].intValue
+      name = json["name"].stringValue
+      userName = json["userName"].stringValue
+    }
+  }
+  
 }
