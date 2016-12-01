317be8028af6db93bc65a0651757b61272f08cad
Lamine Ndiaye <lamine.ndiaye@niji.fr>
2016-12-01 18:18:30 +0100

    Add Factory builder for unit test

---
+++ QontoTest/Classes/ViewControllers/UIViewController+UI.swift
@@ -0,0 +1,38 @@
+//
+//  UITableViewControlelr+AlertView.swift
+//  QontoTest
+//
+//  Created by Lamine NDIAYE on 01/12/2016.
+//  Copyright © 2016 Lamine. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+  
+  func showAlert(title: String?, message: String?) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    
+    let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+      self.dismiss(animated: true, completion: nil)
+    }
+    alertController.addAction(OKAction)
+    
+    self.present(alertController, animated: true) {
+    }
+  }
+  
+  func addIndicator() {
+    let blueIndicator = UIActivityIndicatorView()
+    blueIndicator.color = UIColor.black
+    blueIndicator.startAnimating()
+    let activityBarButton = UIBarButtonItem(customView: blueIndicator)
+    navigationItem.setLeftBarButton(activityBarButton, animated: true)
+  }
+  
+  func removeBlueIndicator() {
+    navigationItem.setLeftBarButton(nil, animated: true)
+  }
+  
+}
---
+++ QontoTestTests/Builders/BuilMe.swift
@@ -0,0 +1,13 @@
+//
+//  BuilMe.swift
+//  QontoTest
+//
+//  Created by Lamine NDIAYE on 01/12/2016.
+//  Copyright © 2016 Lamine. All rights reserved.
+//
+
+import UIKit
+
+class BuilMe: NSObject {
+
+}
